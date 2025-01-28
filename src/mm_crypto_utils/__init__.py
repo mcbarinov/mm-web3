977aef38230c32ea92711ac000505c7860ff7fd7
mcbarinov <mcbarinov@proton.me>
2025-01-28 15:09:32 +0300

    fetch_proxies_or_fatal

--- src/mm_crypto_utils/__init__.py
+++ src/mm_crypto_utils/__init__.py
@@ -1,9 +1,10 @@
 from .account import AddressToPrivate as AddressToPrivate
-from .account import TxRoute as TxRoute
 from .calcs import calc_decimal_value as calc_decimal_value
 from .log import get_log_prefix as get_log_prefix
 from .log import init_logger as init_logger
 from .node import Nodes as Nodes
 from .node import random_node as random_node
 from .proxy import Proxies as Proxies
+from .proxy import fetch_proxies_or_fatal as fetch_proxies_or_fatal
 from .proxy import random_proxy as random_proxy
+from .tx_route import TxRoute as TxRoute
