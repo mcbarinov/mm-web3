ef070c864484efe60ab22e77773ca9ca7a81225a
mcbarinov <mcbarinov@proton.me>
2025-04-22 09:56:36 +0300

    Network.explorer

--- src/mm_crypto_utils/constants.py
+++ src/mm_crypto_utils/constants.py
@@ -57,6 +57,92 @@ class Network(str, Enum):
             return NetworkType.STARKNET
         raise ValueError("no network found")
 
+    def explorer_token(self, token: str) -> str:
+        match self:
+            case Network.ARBITRUM_ONE:
+                return f"https://arbiscan.io/token/{token}"
+            case Network.AVAX_C:
+                return f"https://snowtrace.io/token/{token}"
+            case Network.APTOS:
+                return f"https://explorer.aptoslabs.com/coin/{token}"
+            case Network.BASE:
+                return f"https://basescan.org/token/{token}"
+            case Network.BSC:
+                return f"https://bscscan.com/token/{token}"
+            case Network.CELO:
+                return f"https://celoscan.io/token/{token}"
+            case Network.CORE:
+                return f"https://scan.coredao.org/token/{token}"
+            case Network.ETHEREUM:
+                return f"https://etherscan.io/token/{token}"
+            case Network.FANTOM:
+                return f"https://ftmscan.com/token/{token}"
+            case Network.LINEA:
+                return f"https://lineascan.build/token/{token}"
+            case Network.OPBNB:
+                return f"https://opbnbscan.com/token/{token}"
+            case Network.OP_MAINNET:
+                return f"https://optimistic.etherscan.io/token/{token}"
+            case Network.POLYGON:
+                return f"https://polygonscan.com/token/{token}"
+            case Network.POLYGON_ZKEVM:
+                return f"https://zkevm.polygonscan.com/token/{token}"
+            case Network.SCROLL:
+                return f"https://scrollscan.com/token/{token}"
+            case Network.SOLANA:
+                return f"https://solscan.io/token/{token}"
+            case Network.STARKNET:
+                return f"https://voyager.online/token/{token}"
+            case Network.ZKSYNC_ERA:
+                return f"https://explorer.zksync.io/token/{token}"
+            case Network.ZORA:
+                return f"https://explorer.zora.energy/tokens/{token}"
+
+        raise ValueError("no network found")
+
+    def explorer_account(self, account: str) -> str:
+        match self:
+            case Network.ARBITRUM_ONE:
+                return f"https://arbiscan.io/address/{account}"
+            case Network.AVAX_C:
+                return f"https://snowtrace.io/address/{account}"
+            case Network.APTOS:
+                return f"https://explorer.aptoslabs.com/account/{account}"
+            case Network.BASE:
+                return f"https://basescan.org/address/{account}"
+            case Network.BSC:
+                return f"https://bscscan.com/address/{account}"
+            case Network.CELO:
+                return f"https://celoscan.io/address/{account}"
+            case Network.CORE:
+                return f"https://scan.coredao.org/address/{account}"
+            case Network.ETHEREUM:
+                return f"https://etherscan.io/address/{account}"
+            case Network.FANTOM:
+                return f"https://ftmscan.com/address/{account}"
+            case Network.LINEA:
+                return f"https://lineascan.build/address/{account}"
+            case Network.OPBNB:
+                return f"https://opbnbscan.com/address/{account}"
+            case Network.OP_MAINNET:
+                return f"https://optimistic.etherscan.io/address/{account}"
+            case Network.POLYGON:
+                return f"https://polygonscan.com/address/{account}"
+            case Network.POLYGON_ZKEVM:
+                return f"https://zkevm.polygonscan.com/address/{account}"
+            case Network.SCROLL:
+                return f"https://scrollscan.com/address/{account}"
+            case Network.SOLANA:
+                return f"https://solscan.io/account/{account}"
+            case Network.ZKSYNC_ERA:
+                return f"https://explorer.zksync.io/address/{account}"
+            case Network.STARKNET:
+                return f"https://voyager.online/contract/{account}"
+            case Network.ZORA:
+                return f"https://explorer.zora.energy/address/{account}"
+
+        raise ValueError("no network found")
+
     @classmethod
     def evm_networks(cls) -> list[Network]:
         return [
