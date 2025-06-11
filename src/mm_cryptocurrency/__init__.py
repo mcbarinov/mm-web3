5ce30b9cb66ff002c329e9c9176fb8728fe05ae3
mcbarinov <mcbarinov@proton.me>
2025-06-11 11:34:38 +0300

    init_loguru

--- src/mm_cryptocurrency/__init__.py
+++ src/mm_cryptocurrency/__init__.py
@@ -3,6 +3,7 @@ from mm_cryptocurrency.calcs import calc_decimal_expression as calc_decimal_expr
 from mm_cryptocurrency.calcs import calc_expression_with_vars as calc_expression_with_vars
 from mm_cryptocurrency.calcs import convert_value_with_units as convert_value_with_units
 from mm_cryptocurrency.config import CryptocurrencyConfig as CryptocurrencyConfig
+from mm_cryptocurrency.log import init_loguru as init_loguru
 from mm_cryptocurrency.network import Network as Network
 from mm_cryptocurrency.network import NetworkType as NetworkType
 from mm_cryptocurrency.node import Nodes as Nodes
